07a6ed79b08ea62ca807076d948f0e0368e27bbb
Camila Ferreira Ribeiro <camila.taruma@gmail.com>
2020-11-10 18:47:36 -0300

    Componente Home
    Criação do Componente Home

--- spaangular/src/app/app.module.ts
+++ spaangular/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
