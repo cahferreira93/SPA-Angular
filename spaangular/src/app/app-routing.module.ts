31198a56cf285b4a7491ddcaf147bca0e43991ea
Camila Ferreira Ribeiro <camila.taruma@gmail.com>
2020-11-11 19:30:04 -0300

    Inserir o componente professor

--- spaangular/src/app/app-routing.module.ts
+++ spaangular/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AlunosComponent } from './alunos/alunos.component';
 import { HomeComponent } from './home/home.component';
+import { ProfessoresComponent } from './professores/professores.component';
 
 const routes: Routes = [
   //Home
@@ -13,6 +14,11 @@ const routes: Routes = [
   {
     path: 'alunos',
     component: AlunosComponent
+  },
+  //Professor
+  {
+    path: 'professores',
+    component: ProfessoresComponent
   }
 ];
 
