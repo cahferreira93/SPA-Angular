043762b1119a284116b2acd4cd87260a2d535632
Camila Ferreira Ribeiro <camila.taruma@gmail.com>
2020-11-10 23:52:49 -0300

    Criando componente Home

--- spaangular/src/app/app.module.ts
+++ spaangular/src/app/app.module.ts
@@ -5,10 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
-
-import {MatTableModule} from '@angular/material/table';
-import {MatTableHarness} from '@angular/material/table/testing';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
   declarations: [
@@ -19,9 +18,9 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatTableModule,
-    MatTableHarness,
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
